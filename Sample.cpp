417abb9e1107fc226a7fcc8ee9b2cf86e417a89e
ivan-lau <ivanlau@programmer.net>
2023-07-02 17:53:46 +0800

    Create Sample.cpp

---
+++ Sample.cpp
@@ -0,0 +1,420 @@
+#include <string>
+#include <cstdarg>
+#include <cstdio>
+#include <benchmark/benchmark.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <random>
+
+enum LogLevel {
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARN,
+    LOG_ERROR
+};
+
+class LogCaller {
+public:
+    void log_remote(unsigned char level, const char* format, const char* message) {
+        // fmt::print("[{}] {}\n", level, message);
+    }
+
+    void log(unsigned char level, const char* format, const char* message) {
+        // fmt::print("[{}] {}\n", level, message);
+    }
+};
+
+
+class Logger1 {
+public:
+    Logger1(std::string name) : name(name) {}
+
+    void info(const char* format, ...) const {
+        va_list arglist;
+        va_start(arglist, format);
+        char buffer[2048];
+        std::vsnprintf(buffer, sizeof(buffer), format, arglist);
+        va_end(arglist);
+        std::string message = makeLog(buffer);
+        sendLog(LOG_INFO, message);
+    }
+
+private:
+    std::string name;
+    std::unique_ptr<LogCaller> logCaller = std::make_unique<LogCaller>();
+
+    std::string makeLog(std::string message) const {
+        return "[" + name + "] " + message + "\n";
+    }
+
+    void sendLog(unsigned char level, std::string const& message) const {
+        if (logCaller) {
+            if (message.size() < 512) {
+                logCaller->log_remote(level, "%s", message.c_str());
+            } else {
+                auto const truncatedMessage = message.substr(0, 506) + " ... \n";
+                logCaller->log_remote(level, "%s", truncatedMessage.c_str());
+                logCaller->log(level, "%s", message.c_str());
+            }
+        }
+    }
+};
+
+class Logger2 {
+public:
+    Logger2(std::string name) : name(std::move(name)) {}
+
+    template<typename... Args>
+    void logMessage(unsigned char level, std::string_view format, Args&&... args) const noexcept {
+        char buffer[kMaxLogMessageSize];
+        auto end = fmt::format_to_n(buffer, kMaxLogMessageSize, format, std::forward<Args>(args)...);
+        if (end.out > buffer) {
+            std::string_view message(buffer, end.out - buffer);
+            sendLog(level, fmt::format("[{}] {}\n", name, message));
+        }
+
+    }
+
+    template<typename... Args>
+    void info(std::string_view format, Args&&... args) const noexcept {
+        logMessage(LOG_INFO, format, std::forward<Args>(args)...);
+    }
+
+private:
+    static constexpr std::size_t kMaxLogMessageSize = 2048;
+    std::string name;
+    std::unique_ptr<LogCaller> logCaller = std::make_unique<LogCaller>();
+
+    void sendLog(unsigned char level, std::string_view message) const noexcept {
+        if (logCaller) {
+            if (message.size() < 512) {
+                logCaller->log_remote(level, "%s", message.data());
+            } else {
+                auto const truncatedMessage = fmt::format( "{:.506} ... \n", message);
+                logCaller->log_remote(level, "%s", truncatedMessage.c_str());
+                logCaller->log(level, "%s", message.data());
+            }
+        }
+    }
+};
+
+// int main() {
+//     // Logger1 logger1("MyLogger1");
+//     // logger1.info("This is a test log message with an integer: %d", 42);
+//     Logger2 logger2("MyLogger2");
+//     logger2.info("This is a test log message with an integer: {}", 42);
+
+
+//     std::string longMessage(1024 * 1024, '-');
+//     longMessage += "\n";
+//     longMessage += "Additional text to make the message longer.";
+//     // logger1.info(longMessage.c_str());
+//     logger2.info(longMessage.c_str());
+
+//     return 0;
+// }
+
+template <int N>
+static void BM_Logger1InfoN(benchmark::State& state) {
+    Logger1 logger("MyLogger");
+    for (auto _ : state) {
+        logger.info("Test message with {} arguments: ", N);
+        for (int i = 0; i < N; ++i) {
+            logger.info("{}", i);
+        }
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+
+BENCHMARK_TEMPLATE(BM_Logger1InfoN, 1);
+BENCHMARK_TEMPLATE(BM_Logger1InfoN, 4);
+BENCHMARK_TEMPLATE(BM_Logger1InfoN, 8);
+BENCHMARK_TEMPLATE(BM_Logger1InfoN, 16);
+
+template <int N>
+static void BM_Logger2InfoN(benchmark::State& state) {
+    Logger2 logger("MyLogger");
+    for (auto _ : state) {
+        logger.info("Test message with %d arguments: ", N);
+        for (int i = 0; i < N; ++i) {
+            logger.info("{}", i);
+        }
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+
+BENCHMARK_TEMPLATE(BM_Logger2InfoN, 1);
+BENCHMARK_TEMPLATE(BM_Logger2InfoN, 4);
+BENCHMARK_TEMPLATE(BM_Logger2InfoN, 8);
+BENCHMARK_TEMPLATE(BM_Logger2InfoN, 16);
+
+
+static void BM_Logger1_double(benchmark::State& state) {
+    Logger1 logger("test_logger");
+
+    for (auto _ : state) {
+        logger.info("The value is %f", 3.14);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_double);
+
+static void BM_Logger2_double(benchmark::State& state) {
+    Logger2 logger("test_logger");
+
+    for (auto _ : state) {
+        logger.info("The value is {}", 3.14);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_double);
+
+static void BM_Logger1_float(benchmark::State& state) {
+    Logger1 logger("test_logger");
+
+    for (auto _ : state) {
+        logger.info("The value is %f", 3.14f);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_float);
+
+static void BM_Logger2_float(benchmark::State& state) {
+    Logger2 logger("test_logger");
+
+    for (auto _ : state) {
+        logger.info("The value is {}", 3.14f);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_float);
+
+static void BM_Logger1_int(benchmark::State& state) {
+    Logger1 logger("test_logger");
+
+    for (auto _ : state) {
+        logger.info("The value is %d", 42);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_int);
+
+static void BM_Logger2_int(benchmark::State& state) {
+    Logger2 logger("test_logger");
+
+    for (auto _ : state) {
+        logger.info("The value is {}", 42);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_int);
+
+static void BM_Logger1_multipleTypes(benchmark::State& state) {
+    Logger1 logger("test_logger");
+
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> int_dist(0, 1000);
+    std::uniform_real_distribution<double> double_dist(0.0, 1.0);
+    std::uniform_real_distribution<float> float_dist(0.0f, 1.0f);
+    std::string chars = "abcdefghijklmnopqrstuvwxyz";
+    std::uniform_int_distribution<int> string_dist(0, chars.size() - 1);
+
+    for (auto _ : state) {
+        int i = int_dist(gen);
+        double d = double_dist(gen);
+        float f = float_dist(gen);
+        std::string s(10, ' ');
+        std::generate_n(s.begin(), s.size(), [&]() { return chars[string_dist(gen)]; });
+
+        logger.info("i=%d d=%f f=%f s=%s", i, d, f, s.c_str());
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_multipleTypes);
+
+static void BM_Logger2_multipleTypes(benchmark::State& state) {
+    Logger2 logger("test_logger");
+
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> int_dist(0, 1000);
+    std::uniform_real_distribution<double> double_dist(0.0, 1.0);
+    std::uniform_real_distribution<float> float_dist(0.0f, 1.0f);
+    std::string chars = "abcdefghijklmnopqrstuvwxyz";
+    std::uniform_int_distribution<int> string_dist(0, chars.size() - 1);
+
+    for (auto _ : state) {
+        int i = int_dist(gen);
+        double d = double_dist(gen);
+        float f = float_dist(gen);
+        std::string s(10, ' ');
+        std::generate_n(s.begin(), s.size(), [&]() { return chars[string_dist(gen)]; });
+
+        logger.info("i={} d={} f={} s={}", i, d, f, s);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_multipleTypes);
+
+static void BM_Logger1_long_args(benchmark::State& state) {
+    Logger1 logger("test_logger");
+
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> int_dist(0, 1000);
+    std::uniform_real_distribution<double> double_dist(0.0, 1.0);
+    std::uniform_real_distribution<float> float_dist(0.0f, 1.0f);
+    std::string chars = "abcdefghijklmnopqrstuvwxyz";
+    std::uniform_int_distribution<int> string_dist(0, chars.size() - 1);
+
+    for (auto _ : state) {
+        int i1 = int_dist(gen);
+        int i2 = int_dist(gen);
+        int i3 = int_dist(gen);
+        double d1 = double_dist(gen);
+        double d2 = double_dist(gen);
+        double d3 = double_dist(gen);
+        float f1 = float_dist(gen);
+        float f2 = float_dist(gen);
+        float f3 = float_dist(gen);
+        std::string s1(10, ' ');
+        std::generate_n(s1.begin(), s1.size(), [&]() { return chars[string_dist(gen)]; });
+        std::string s2(10, ' ');
+        std::generate_n(s2.begin(), s2.size(), [&]() { return chars[string_dist(gen)]; });
+        std::string s3(10, ' ');
+        std::generate_n(s3.begin(), s3.size(), [&]() { return chars[string_dist(gen)]; });
+
+        logger.info("i1=%d i2=%d i3=%d d1=%f d2=%f d3=%f f1=%f f2=%f f3=%f s1=%s s2=%s s3=%s",
+            i1, i2, i3, d1, d2, d3, f1, f2, f3, s1.c_str(), s2.c_str(), s3.c_str());
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_long_args);
+
+static void BM_Logger2_long_args(benchmark::State& state) {
+    Logger2 logger("test_logger");
+
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> int_dist(0, 1000);
+    std::uniform_real_distribution<double> double_dist(0.0, 1.0);
+    std::uniform_real_distribution<float> float_dist(0.0f, 1.0f);
+    std::string chars = "abcdefghijklmnopqrstuvwxyz";
+    std::uniform_int_distribution<int> string_dist(0, chars.size() - 1);
+
+    for (auto _ : state) {
+        int i1 = int_dist(gen);
+        int i2 = int_dist(gen);
+        int i3 = int_dist(gen);
+        double d1 = double_dist(gen);
+        double d2 = double_dist(gen);
+        double d3 = double_dist(gen);
+        float f1 = float_dist(gen);
+        float f2 = float_dist(gen);
+        float f3 = float_dist(gen);
+        std::string s1(10, ' ');
+        std::generate_n(s1.begin(), s1.size(), [&]() { return chars[string_dist(gen)]; });
+        std::string s2(10, ' ');
+        std::generate_n(s2.begin(), s2.size(), [&]() { return chars[string_dist(gen)]; });
+        std::string s3(10, ' ');
+        std::generate_n(s3.begin(), s3.size(), [&]() { return chars[string_dist(gen)]; });
+
+        logger.info("i1={} i2={} i3={} d1={} d2={} d3={} f1={} f2={} f3={} s1={} s2={} s3={}",
+            i1, i2, i3, d1, d2, d3, f1, f2, f3, s1, s2, s3);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_long_args);
+
+static void BM_Logger1_string_short(benchmark::State& state) {
+    Logger1 logger("test_logger");
+    std::string message = "This is a short message";
+
+    for (auto _ : state) {
+        logger.info("%s", message.c_str());
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_string_short);
+
+static void BM_Logger2_string_short(benchmark::State& state) {
+    Logger2 logger("test_logger");
+    std::string message = "This is a short message";
+
+    for (auto _ : state) {
+        logger.info("{}", message);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_string_short);
+
+static void BM_Logger1_string_longButLessThanBuf(benchmark::State& state) {
+    Logger1 logger("test_logger");
+    std::string message(1024, 'x');
+
+    for (auto _ : state) {
+        logger.info("%s", message.c_str());
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger1_string_longButLessThanBuf);
+
+static void BM_Logger2_string_longButLessThanBuf(benchmark::State& state) {
+    Logger2 logger("test_logger");
+    std::string message(1024, 'x');
+
+    for (auto _ : state) {
+        logger.info("{}", message);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_Logger2_string_longButLessThanBuf);
+
+
+static void BM_LongFormat1Message(benchmark::State& state) {
+    Logger1 logger("BenchmarkLogger");
+
+    std::string longMessage(1024 * 1024, 'X');
+
+    for (auto _ : state) {
+        logger.info(longMessage.c_str());
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_LongFormat1Message);
+
+static void BM_LongFormat2Message(benchmark::State& state) {
+    Logger2 logger("BenchmarkLogger");
+
+    std::string longMessage(1024 * 1024, 'X');
+
+    for (auto _ : state) {
+        logger.info(longMessage.c_str());
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_LongFormat2Message);
+
+static void BM_LongSingleArg1Message(benchmark::State& state) {
+    Logger1 logger("BenchmarkLogger");
+
+    std::string longMessage(1024 * 1024, 'X');
+
+    for (auto _ : state) {
+        logger.info("This is a test log message with a long string: %s", longMessage);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_LongSingleArg1Message);
+
+static void BM_LongSingleArg2Message(benchmark::State& state) {
+    Logger2 logger("BenchmarkLogger");
+
+    std::string longMessage(1024 * 1024, 'X');
+
+    for (auto _ : state) {
+        logger.info("This is a test log message with a long string: {}", longMessage);
+    }
+    state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_LongSingleArg2Message);
+
+
+BENCHMARK_MAIN();
