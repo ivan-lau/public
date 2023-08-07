497f1d6229438914189cb806a9486b2caea6a4da
ivan-lau <ivanlau@programmer.net>
2023-08-07 21:41:00 +0800

    Update tc.cpp

--- tc.cpp
+++ tc.cpp
@@ -1,30 +1,18 @@
-#include <execinfo.h>
-#include <iostream>
+void printCallerFunctionNames() {
+    const int maxStackSize = 10;  // Maximum stack trace size
+    void* callStack[maxStackSize];
+    int stackSize = backtrace(callStack, maxStackSize);
 
-void printCallerFunctionName() {
-    void* callStack[2];  // Adjust the size as needed
-    int stackSize = backtrace(callStack, 2);  // Adjust the parameter as needed
-
-    if (stackSize >= 2) {
-        // Obtain the symbol name of the previous caller function
-        char** symbols = backtrace_symbols(callStack + 1, 1);  // Adjust the index and count as needed
-
-        if (symbols != nullptr) {
-            std::cout << "Previous caller function: " << symbols[0] << std::endl;
-            std::free(symbols);
-        } else {
-            std::cout << "Failed to obtain caller function name." << std::endl;
+    if (stackSize > 1) {
+        Dl_info dlInfo;
+        for (int i = 1; i < stackSize; ++i) {
+            if (dladdr(callStack[i], &dlInfo) != 0) {
+                std::cout << "Caller function " << (stackSize - i) << ": " << dlInfo.dli_sname << std::endl;
+            } else {
+                std::cout << "Failed to obtain caller function name." << std::endl;
+            }
         }
     } else {
         std::cout << "No caller function information available." << std::endl;
     }
-}
-
-void foo() {
-    printCallerFunctionName();
-}
-
-int main() {
-    foo();
-    return 0;
 }
